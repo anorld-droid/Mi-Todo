0abd3d7398a81d30162a56efb94bd9ed3a99da02
anorld-droid <mulindipatrice00@gmail.com>
2022-04-18 20:04:47 +0300

    added Meals and categories as filter options.

--- app/src/main/java/com/anorlddroid/mi_todo/ui/components/Filters.kt
+++ app/src/main/java/com/anorlddroid/mi_todo/ui/components/Filters.kt
@@ -3,59 +3,147 @@ package com.anorlddroid.mi_todo.ui.components
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Tab
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Sort
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.dp
+import com.anorlddroid.mi_todo.ui.theme.lBlue
+import com.anorlddroid.mi_todo.ui.theme.lRed
+import com.anorlddroid.mi_todo.ui.theme.twitterBlue
 
 @Composable
 fun FilterBar(
     categoriesFilters: List<String>?,
     onFilterSelected: ((String) -> Unit)?,
-    selectedFilter: String?
+    selectedFilter: String?,
 ) {
-    if (categoriesFilters?.isNotEmpty() == true) {
-        val filterName by remember { mutableStateOf("Categories") }
-        val selectedIndex = categoriesFilters.indexOfFirst { it == selectedFilter }
-        LazyRow(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(start = 2.dp, end = 8.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 56.dp),
-        ) {
-            item {
+    var showFilters by remember { mutableStateOf(false) }
+    var filterCategory by remember { mutableStateOf("Categories") }
+    val selectedIndex = categoriesFilters?.indexOfFirst { it == selectedFilter }
+
+    LazyRow(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(start = 2.dp, end = 8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp),
+    ) {
+        item {
+            Box {
                 Row(
                     modifier = Modifier
                         .padding(start = 2.dp, end = 6.dp)
+                        .clickable {
+                            showFilters = !showFilters
+                        }
+                        .background(
+                            color = MaterialTheme.colors.background,
+                            shape = MaterialTheme.shapes.medium
+                        )
                 ) {
-                    Icon(
-                        imageVector = Icons.Filled.Sort,
-                        contentDescription = "Sort",
-                    )
                     Text(
-                        text = filterName,
+                        text = filterCategory,
                         style = MaterialTheme.typography.h6,
-                        color = MaterialTheme.colors.secondary
+                        color = MaterialTheme.colors.secondary,
+                        modifier = Modifier.padding(start = 10.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.ArrowDropDown,
+                        tint = MaterialTheme.colors.secondary,
+                        contentDescription = "Sort",
+                        modifier = Modifier.padding(horizontal = 1.dp)
                     )
                 }
-                categoriesFilters.forEachIndexed { index, filter ->
+                Row(modifier = Modifier.padding(start = 10.dp)) {
+                    MaterialTheme(
+                        shapes = MaterialTheme.shapes.copy(
+                            medium = RoundedCornerShape(
+                                16.dp
+                            )
+                        )
+                    ) {
+                        DropdownMenu(
+                            modifier = Modifier
+                                .background(
+                                    color = MaterialTheme.colors.onPrimary
+                                )
+                                .width(200.dp),
+                            expanded = showFilters,
+                            onDismissRequest = { showFilters = false },
+
+                            ) {
+                            DropdownMenuItem(onClick = {
+                                showFilters = false
+                                filterCategory = "Categories"
+                                if (onFilterSelected != null) {
+                                    if (categoriesFilters != null) {
+                                        onFilterSelected(categoriesFilters[0])
+                                    }
+                                }
+
+                            }) {
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = "Categories",
+                                        style = MaterialTheme.typography.h6,
+                                        color = if (filterCategory == "Categories") twitterBlue else MaterialTheme.colors.secondary
+                                    )
+                                    if (filterCategory == "Categories") {
+                                        Icon(
+                                            Icons.Filled.Done,
+                                            tint = twitterBlue,
+                                            contentDescription = null,
+                                        )
+                                    }
+                                }
+                            }
+                            DropdownMenuItem(onClick = {
+                                showFilters = false
+                                filterCategory = "Meals"
+                                if (onFilterSelected != null) {
+                                    onFilterSelected("Meals")
+                                }
+                            }) {
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        text = "Meals",
+                                        style = MaterialTheme.typography.h6,
+                                        color = if (filterCategory == "Meals") twitterBlue else MaterialTheme.colors.secondary
+                                    )
+                                    if (filterCategory == "Meals") {
+                                        Icon(
+                                            Icons.Filled.Done,
+                                            tint = twitterBlue,
+                                            contentDescription = null
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (filterCategory == "Categories") {
+                categoriesFilters?.forEachIndexed { index, filter ->
                     Tab(
                         selected = index == selectedIndex,
                         modifier = Modifier.background(
@@ -73,7 +161,6 @@ fun FilterBar(
                             selected = index == selectedIndex
                         )
                     }
-
                 }
             }
         }
@@ -89,7 +176,6 @@ fun FilterChip(
     shape: Shape = MaterialTheme.shapes.small
 ) {
 
-
     val backgroundColor by animateColorAsState(
         if (selected) MaterialTheme.colors.primary
         else MaterialTheme.colors.background
@@ -97,7 +183,7 @@ fun FilterChip(
 
     val border = Modifier.fadeInDiagonalGradientBorder(
         showBorder = !selected,
-        colors = listOf(Color(0xFF16bffd), Color(0xFFcb3066)),
+        colors = listOf(lBlue, lRed),
         shape = shape
     )
     val textColor by animateColorAsState(
